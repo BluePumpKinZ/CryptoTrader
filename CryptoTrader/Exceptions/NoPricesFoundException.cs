b9dcf940d7fd5663e468e27ea52bbd3834b2f4a9
Jonas Leijzen <jonasleijzen@gmail.com>
2021-06-30 18:51:22 +0200

    Added Exception

---
+++ CryptoTrader/Exceptions/NoPricesFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrader.Exceptions {
+
+	public class NoPricesFoundException : SystemException {
+
+		public NoPricesFoundException () : base () { }
+
+		public NoPricesFoundException (string message) : base (message) { }
+
+	}
+
+}
