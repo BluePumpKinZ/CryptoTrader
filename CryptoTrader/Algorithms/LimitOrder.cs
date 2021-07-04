9f8407649e31df93fbab72c00f5d91d0071c604f
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-04 11:04:14 +0200

    Added LimitOrder struct

---
+++ CryptoTrader/Algorithms/LimitOrder.cs
@@ -0,0 +1,23 @@
+﻿using CryptoTrader.NicehashAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrader.Algorithms {
+
+	public struct LimitOrder {
+
+		public Currency Currency { private set; get; }
+		public double Value { private set; get; }
+		public double Price { private set; get; }
+		public OrderType Type { private set; get; }
+
+		public LimitOrder (Currency currency, double value, double price, OrderType type) {
+			Currency = currency;
+			Value = value;
+			Price = price;
+			Type = type;
+		}
+
+	}
+}
