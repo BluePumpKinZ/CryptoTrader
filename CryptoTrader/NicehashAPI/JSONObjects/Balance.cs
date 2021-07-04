18eccc4a9729f9adc3f3b55a91b5bc6def8c025b
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-04 11:07:54 +0200

    Expanded Balance.cs and Balances.cs to work as classes instead of JSONObjects

--- CryptoTrader/NicehashAPI/JSONObjects/Balance.cs
+++ CryptoTrader/NicehashAPI/JSONObjects/Balance.cs
@@ -1,4 +1,4 @@
-﻿using connect;
+﻿using CryptoTrader.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Reflection.PortableExecutable;
@@ -14,9 +14,17 @@ namespace CryptoTrader.NicehashAPI.JSONObjects {
 		public double TotalBalance { get { return Available + Pending; } }
 		public double BTCRate { private set; get; }
 
+		public Balance (Currency c, double available, double pending, double btcRate) {
+			Currency = c;
+			Available = available;
+			Pending = pending;
+			BTCRate = btcRate;
+		}
+
 		public Balance (Currency c, double value, double btcRate) {
 			Currency = c;
 			Available = value;
+			Pending = 0;
 			BTCRate = btcRate;
 		}
 
@@ -27,7 +35,9 @@ namespace CryptoTrader.NicehashAPI.JSONObjects {
 		internal override void ParsePart (string key, string value) {
 			switch (key) {
 			case "currency":
-				Currencies.TryGetCurrencyFromBTCPair (value, out Currency c);
+				bool foundPair = Currencies.TryGetCurrencyFromBTCPair (value, out Currency c);
+				if (!foundPair)
+					c = Currencies.GetCurrencyFromToken (value);
 				Currency = c;
 				break;
 			case "available":
@@ -36,11 +46,52 @@ namespace CryptoTrader.NicehashAPI.JSONObjects {
 			case "pending":
 				Pending = double.Parse (value);
 				break;
+			case "btcRate":
+				BTCRate = double.Parse (value);
+				break;
 			}
 		}
 
-		public double GetBTCValue () {
-			return Available * BTCRate;
+		public Balance ToBTCBalance () {
+			return new Balance (Currency.Bitcoin, Available * BTCRate, Pending * BTCRate, 1);
+		}
+
+		public Balance ToCurrency (Currency currency, double price) {
+			Balance btc = ToBTCBalance ();
+			return new Balance (currency, btc.Available / price, btc.Pending / price, price);
+		}
+
+		public Balance ToCurrencyActual (Currency currency) {
+			if (!PriceWatcher.HasPrices)
+				throw new NoPricesFoundException ("The pricewatcher hasn't been started.");
+			return ToCurrency (currency, PriceWatcher.GetBTCPrice (currency));
+		}
+
+		public void Add (Balance balance) {
+			if (Currency != balance.Currency) {
+				balance = ToCurrency (Currency, BTCRate);
+			}
+			Available += balance.Available;
+			Pending += balance.Pending;
+		}
+
+		public void Subtract (Balance balance) {
+			balance.Available = -balance.Available;
+			balance.Pending = -balance.Pending;
+			Add (balance);
+		}
+
+		public void ApplyFee (double fee) {
+			Available *= 1 - fee;
+			Pending *= 1 - fee;
+		}
+
+		public void UpdateBTCRate () {
+			BTCRate = PriceWatcher.GetBTCPrice (Currency);
+		}
+
+		public bool IsComplete () {
+			return BTCRate != 0;
 		}
 
 		public override string ToString () {
--- CryptoTrader/NicehashAPI/JSONObjects/Balances.cs
+++ CryptoTrader/NicehashAPI/JSONObjects/Balances.cs
@@ -1,47 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CryptoTrader.NicehashAPI.JSONObjects {
-	
+
 	public class Balances : IParsable {
 
-		public Balance balance;
+		public Balance TotalBalance { get { return GetTotalBalance (); } }
 		private readonly List<Balance> balances;
+
+		public Balances () {
+			balances = new List<Balance> ();
+		}
+
+		public Balances (List<Balance> balances) {
+			this.balances = balances;
+		}
 
 		public Balances (string s) {
 			balances = new List<Balance> ();
 			Parse (s);
-			balance = GetTotalBalance ();
 		}
 
 		internal override void ParsePart (string key, string value) {
 			switch (key) {
 			case "currencies":
-				Parse (value);
-				break;
-			default:
-				try {
-					Balance b = new Balance (value);
-					balances.Add (b);
-				} catch (Exception) {
-
+				foreach (string split in GetJSONSplits (value)) {
+					Balance b = new Balance (split);
+					if (b.IsComplete ())
+						balances.Add (b);
 				}
 				break;
 			}
 		}
 
-		private Balance GetTotalBalance () {
-			double totalValue = 0;
-			for (int i = 0; i < balances.Count; i++) {
-				totalValue += balances[i].GetBTCValue ();
+		public Balance GetBalanceForCurrency (Currency currency) {
+			int index = GetBalanceIndex (currency);
+			if (index == -1) {
+				Balance balance = new Balance (currency, 0, 0, PriceWatcher.GetBTCPrice (currency));
+				balances.Add (balance);
+				return balance;
 			}
-			return new Balance (Currency.Bitcoin, totalValue, 1);
+			return balances[index];
+		}
+
+		public void AddBalance (Balance balance) {
+			Balance baseBalance = GetBalanceForCurrency (balance.Currency);
+			baseBalance.Add (balance);
+			SetBalance (baseBalance);
+		}
+
+		public void SubtractBalance (Balance balance) {
+			Balance baseBalance = GetBalanceForCurrency (balance.Currency);
+			baseBalance.Subtract (balance);
+			SetBalance (baseBalance);
+		}
+
+		private void SetBalance (Balance balance) {
+			int index = GetBalanceIndex (balance.Currency);
+			if (index == -1) {
+				balances.Add (balance);
+				return;
+			}
+
+			balances[index] = balance;
+		}
+
+		private int GetBalanceIndex (Currency c) {
+			for (int i = 0; i < balances.Count; i++) {
+				if (balances[i].Currency == c)
+					return i;
+			}
+			return -1;
+		}
+
+		public void UpdateBTCRates () {
+			balances.ForEach ((balance) => balance.UpdateBTCRate ());
+		}
+
+		private Balance GetTotalBalance () {
+			double totalAvailable = 0;
+			double totalPending = 0;
+			for (int i = 0; i < balances.Count; i++) {
+				Balance btcBalance = balances[i].ToBTCBalance ();
+				totalAvailable += btcBalance.Available;
+				totalPending += btcBalance.Pending;
+			}
+			return new Balance (Currency.Bitcoin, totalAvailable, totalPending, 1);
 		}
 
 		public override string ToString () {
 			StringBuilder sb = new StringBuilder ();
-			sb.Append ($"TotalBalance\n\t{balance}\nBalances");
+			sb.Append ($"TotalBalance\n\t{TotalBalance}\nBalances");
 			for (int i = 0; i < balances.Count; i++) {
 				sb.Append ("\n\t" + balances[i].ToString ());
 			}
