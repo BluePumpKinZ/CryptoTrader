d721ae069c8d43bf3629a2106b742f1f21e648a3
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-06 22:07:46 +0200

    Added class for offloading heavy AI processing tasks to worker threads.

---
+++ CryptoTrader/AISystem/AIProcessTaskScheduler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace CryptoTrader.AISystem {
+
+	public static class AIProcessTaskScheduler {
+
+		public static ConcurrentQueue<Action> tasks = new ConcurrentQueue<Action> ();
+		private static bool stopWorkerThreads = true;
+		private static bool hasJoined = false;
+		private static Thread[] workerThreads;
+		private static int threadCount = 1;
+
+		public static void StartExecuting () {
+			stopWorkerThreads = false;
+			hasJoined = false;
+			workerThreads = new Thread[threadCount];
+			ThreadStart threadStart = new ThreadStart (delegate { WorkerThread (); });
+			Array.ForEach (workerThreads, (workerThread) => workerThread = new Thread (threadStart));
+			Array.ForEach (workerThreads, (workerThread) => workerThread.Start ());
+		}
+
+		public static void SetThreadCount (int threads) {
+			bool running = !stopWorkerThreads;
+			int min = 1;
+			int max = Environment.ProcessorCount;
+			if (min > threads || threads > max) {
+				throw new ArgumentException ($"Couldn't set threadcount: number of threads should be between {min} and {max}.");
+			}
+			if (running) {
+				StopExecuting ();
+				FinishTasks ();
+				threadCount = threads;
+				StartExecuting ();
+			}
+		}
+
+		public static void StopExecuting () {
+			stopWorkerThreads = true;
+		}
+
+		/// <summary>
+		/// Joins the worker thread(s) until all tasks have been completed
+		/// </summary>
+		public static void FinishTasks () {
+			try {
+				hasJoined = true;
+				Array.ForEach (workerThreads, (workerThread) => workerThread.Join ());
+			} catch (NullReferenceException) { } finally {
+				hasJoined = false;
+			}
+		}
+
+		public static void AddTask (Action action) {
+			tasks.Enqueue (action);
+		}
+
+		private static void WorkerThread () {
+			while (!stopWorkerThreads) {
+				while (tasks.TryDequeue (out Action task)) {
+					task.Invoke ();
+					if (stopWorkerThreads)
+						return;
+				}
+				if (hasJoined)
+					return;
+				Thread.Sleep (1);
+			}
+		}
+	}
+
+}
