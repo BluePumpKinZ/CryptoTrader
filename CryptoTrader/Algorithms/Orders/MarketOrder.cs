da84f18c3faea8aa81f942c8e9db63c910d80097
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-22 21:00:37 +0200

    Added ToString to MarketOrder.cs

--- CryptoTrader/Algorithms/Orders/MarketOrder.cs
+++ CryptoTrader/Algorithms/Orders/MarketOrder.cs
@@ -10,5 +10,9 @@ namespace CryptoTrader.Algorithms.Orders {
 			return base.GetOrderUrl () + $"&secQuantity={0.9 * Value}";
 		}
 
+		public override string ToString () {
+			return $"Market{(IsBuyOrder ? "Buy" : "Sell")}Order (Time: {Time} Currency: {Currency} Value: {Value})";
+		}
+
 	}
 }
