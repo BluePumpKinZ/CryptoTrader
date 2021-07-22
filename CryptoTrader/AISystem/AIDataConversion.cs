be9b442b14b359291e2b1fbfd9b45e8c954bd085
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-22 22:53:48 +0200

    Added AIDataConversion class

---
+++ CryptoTrader/AISystem/AIDataConversion.cs
@@ -0,0 +1,42 @@
+﻿using CryptoTrader.Algorithms.Orders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrader.AISystem {
+
+	public static class AIDataConversion {
+
+		public static Tuple<double[], double[]> GetBuyAndSellsFromOrders (PriceGraph graph, MarketOrder[] orders) {
+
+			int length = graph.GetLength ();
+
+			double[] buys = new double[length];
+			double[] sells = new double[length];
+
+			int orderCount = 0;
+			MarketOrder order = orders[0];
+
+			for (int i = 0; i < length; i++) {
+				long time = graph.GetTimeByIndex (i);
+
+				if (order.Time > time)
+					continue;
+
+				if (order.IsBuyOrder)
+					buys[i] = 1;
+				else
+					sells[i] = 1;
+
+				if (orderCount + 1 >= orders.Length)
+					break;
+				
+				order = orders[++orderCount];
+			}
+
+			return new Tuple<double[], double[]> (buys, sells);
+		}
+
+	}
+
+}
