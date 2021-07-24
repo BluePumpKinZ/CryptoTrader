a29c4b321971ef5e1206707eaa843c980ec16e8e
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-24 23:25:15 +0200

    Created template AI algorithm class.

---
+++ CryptoTrader/Algorithms/AlgoAI.cs
@@ -0,0 +1,18 @@
+﻿using CryptoTrader.AISystem;
+using CryptoTrader.NicehashAPI.JSONObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrader.Algorithms {
+
+	public class AlgoAI : Algorithm {
+
+		public DeepLearningNetwork[] networks;
+
+		internal override void IterateInternal (PriceGraph graph, ref Balances balances) {
+			throw new NotImplementedException ();
+		}
+
+	}
+}
