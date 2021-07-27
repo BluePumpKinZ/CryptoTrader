cb6dbdddc3bf9f4777e4706e952d937b6bd13507
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-27 23:55:09 +0200

    Added algorithm object creation helper functions

---
+++ CryptoTrader/Utils/TypeMapping.cs
@@ -0,0 +1,23 @@
+﻿using CryptoTrader.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryptoTrader.Utils {
+
+	public static class TypeMapping {
+
+		public static Algorithm AlgorithmFromName (string algoName) {
+			Type type = Type.GetType ($"CryptoTrader.Algorithms.{algoName}");
+			return Activator.CreateInstance (type) as Algorithm;
+		}
+
+		public static string NameFromAlgorithm (Algorithm algorithm) {
+			Console.WriteLine (algorithm.GetType ());
+			return algorithm.GetType ().Name;
+		}
+
+	}
+
+}
