7de22eff98984a92a907ba42de880768444c70f4
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-28 11:01:33 +0200

    Added algorithm storage to Trader

--- CryptoTrader/Trader.cs
+++ CryptoTrader/Trader.cs
@@ -1,14 +1,67 @@
-﻿using CryptoTrader.Keys;
+﻿using CryptoTrader.Algorithms;
+using CryptoTrader.Keys;
 using CryptoTrader.NicehashAPI;
+using CryptoTrader.Utils;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CryptoTrader {
 
 	public static class Trader {
 
+		private static List<Algorithm> algorithms = new List<Algorithm> ();
+
 		public static void Initialize () {
 			AppDomain.CurrentDomain.ProcessExit += new EventHandler (AutoSavePrices);
+			// LoadAlgorithms ();
+		}
+
+		public static void LoadAlgorithms (string path) {
+			if (!File.Exists (path)) {
+				Console.WriteLine ("Trader algorithms could not be loaded, make sure the given path is valid");
+			}
+
+			byte[] bytes = File.ReadAllBytes (path);
+			algorithms.Clear ();
+
+			int byteIndex = 0;
+			int algoCount = BitConverter.ToInt32 (bytes, byteIndex);
+			byteIndex += 4;
+			for (int i = 0; i < algoCount; i++) {
+				int algoNameLength = BitConverter.ToInt32 (bytes, byteIndex);
+				byteIndex += 4;
+
+				byte[] algoNameBytes = new byte[algoNameLength];
+				Array.Copy (bytes, byteIndex, algoNameBytes, 0, algoNameLength);
+				byteIndex += algoNameLength;
+
+				int algoLength = BitConverter.ToInt32 (bytes, byteIndex);
+				byteIndex += 4;
+
+				byte[] algoBytes = new byte[algoLength];
+				Array.Copy (bytes, byteIndex, algoNameBytes, 0, algoLength);
+				byteIndex += algoLength;
+
+				Algorithm algo = TypeMapping.AlgorithmFromName (TypeMapping.BytesToString (algoNameBytes));
+				algo.LoadFromBytes (algoBytes);
+				algorithms.Add (algo);
+			}
+		}
+
+		public static void SaveAlgorithms (string path) {
+			List<byte> bytes = new List<byte> ();
+			bytes.AddRange (BitConverter.GetBytes(algorithms.Count));
+			algorithms.ForEach ((t) => {
+				byte[] algoNameBytes = TypeMapping.NameToBytes (TypeMapping.NameFromAlgorithm (t));
+				byte[] algoBytes = t.SaveToBytes ();
+				bytes.AddRange (BitConverter.GetBytes (algoNameBytes.Length));
+				bytes.AddRange (algoNameBytes);
+				bytes.AddRange (BitConverter.GetBytes (algoBytes.Length));
+				bytes.AddRange (algoBytes);
+			});
+			File.WriteAllBytes (path, bytes.ToArray ());
 		}
 
 		public static void Start () {
