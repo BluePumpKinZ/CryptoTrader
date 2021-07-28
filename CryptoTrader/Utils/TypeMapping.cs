1d6e9f03ceab7720a181004ab837afcc2fdb7d03
Jonas Leijzen <jonasleijzen@gmail.com>
2021-07-28 10:43:06 +0200

    Added type conversion to TypeMapping

--- CryptoTrader/Utils/TypeMapping.cs
+++ CryptoTrader/Utils/TypeMapping.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoTrader.Algorithms;
 using System;
+using System.Text;
 
 namespace CryptoTrader.Utils {
 
@@ -13,6 +14,14 @@ namespace CryptoTrader.Utils {
 		public static string NameFromAlgorithm (Algorithm algorithm) {
 			Console.WriteLine (algorithm.GetType ());
 			return algorithm.GetType ().Name;
+		}
+
+		public static byte[] NameToBytes (string name) {
+			return Encoding.UTF8.GetBytes (name);
+		}
+
+		public static string BytesToString (byte[] bytes) {
+			return Encoding.UTF8.GetString (bytes);
 		}
 
 	}
