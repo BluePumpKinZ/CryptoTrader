109f4b5329b233497ba42116f89fefad2d82780c
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-01 19:04:29 +0200

    Added System.Array.GetRange

---
+++ CryptoTrader/Utils/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CryptoTrader.Utils {
+
+	public static class Extensions {
+
+		public static T[] GetRange<T> (this Array array, int startIndex, int length) {
+			T[] copyTo = new T[length];
+			Array.Copy (array, startIndex, copyTo, 0, length);
+			return copyTo;
+		}
+
+	}
+
+}
