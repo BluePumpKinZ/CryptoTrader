de0b5b26f1005bcc225070fab42d14c833b2e0f1
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-01 19:07:30 +0200

    Updated function header to allow for type inferring

--- CryptoTrader/Utils/Extensions.cs
+++ CryptoTrader/Utils/Extensions.cs
@@ -4,7 +4,7 @@ namespace CryptoTrader.Utils {
 
 	public static class Extensions {
 
-		public static T[] GetRange<T> (this Array array, int startIndex, int length) {
+		public static T[] GetRange<T> (this T[] array, int startIndex, int length) {
 			T[] copyTo = new T[length];
 			Array.Copy (array, startIndex, copyTo, 0, length);
 			return copyTo;
