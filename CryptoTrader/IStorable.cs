b4517326b0772809088e5655f9bef199f23fa390
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-16 13:27:36 +0200

    Made helper functions static

--- CryptoTrader/IStorable.cs
+++ CryptoTrader/IStorable.cs
@@ -11,7 +11,7 @@ namespace CryptoTrader {
 
 		public abstract void SaveToBytes (ref List<byte> datalist);
 
-		private protected byte[] GetDataRange (ref int index, byte[] data) {
+		internal static byte[] GetDataRange (ref int index, byte[] data) {
 			int length = BitConverter.ToInt32 (data, 0);
 			index += 4;
 			byte[] newData = data.GetRange (index + 4, length);
@@ -19,7 +19,7 @@ namespace CryptoTrader {
 			return newData;
 		}
 
-		private protected void AddData (ref List<byte> datalist, byte[] data) {
+		internal static void AddData (ref List<byte> datalist, byte[] data) {
 			int length = data.Length;
 			datalist.AddRange (BitConverter.GetBytes (length));
 			datalist.AddRange (data);
