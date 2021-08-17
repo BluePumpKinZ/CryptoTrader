94e46b7643ba230cc8411b207495f728cb897c92
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-17 22:32:34 +0200

    Make threadcount change when not running

--- CryptoTrader/AISystem/AIProcessTaskScheduler.cs
+++ CryptoTrader/AISystem/AIProcessTaskScheduler.cs
@@ -35,7 +35,8 @@ namespace CryptoTrader.AISystem {
 				FinishTasks ();
 				ThreadCount = threads;
 				StartExecuting ();
-			}
+			} else
+				ThreadCount = threads;
 		}
 
 		public static void StopExecuting () {
