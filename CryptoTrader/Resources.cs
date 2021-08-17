d768cbbe246d873731699174a2a89de790d07ca2
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-17 16:53:19 +0200

    Added resource loading

---
+++ CryptoTrader/Resources.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CryptoTrader {
+
+	public static class Resources {
+
+		public static string ReadResource (string name) {
+			Assembly assembly = Assembly.GetExecutingAssembly ();
+			string resourceName = assembly.GetManifestResourceNames ().Single (str => str.EndsWith(name));
+
+			using (Stream stream = assembly.GetManifestResourceStream (resourceName))
+			using (StreamReader reader = new StreamReader (stream))
+				return reader.ReadToEnd ();
+		}
+
+	}
+
+}
