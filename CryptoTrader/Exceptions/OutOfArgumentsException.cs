971df1b5db8abe4c7b1ad5b46f921bc83655d152
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-17 14:19:51 +0200

    Made new exception derived from SystemException

---
+++ CryptoTrader/Exceptions/OutOfArgumentsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrader.Exceptions {
+
+	public class OutOfArgumentsException : SystemException {
+
+		public OutOfArgumentsException () : base () { }
+
+		public OutOfArgumentsException (string message) : base (message) { }
+
+	}
+
+}
