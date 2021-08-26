66749719c8de8be7eafc136753c2aa6579d19c75
Jonas Leijzen <jonasleijzen@gmail.com>
2021-08-26 15:52:20 +0200

    Added way to get proper format for currency values

---
+++ CryptoTrader/Utils/NumberFormatting.cs
@@ -0,0 +1,17 @@
+﻿using CryptoTrader.NicehashAPI;
+using CryptoTrader.NicehashAPI.JSONObjects;
+
+namespace CryptoTrader.Utils {
+
+	public static class NumberFormatting {
+
+		public static string FormatAmount (double value, Currency currency) {
+
+			MarketStatus marketStatus = PriceWatcher.ExchangeStatus.GetStatusForCurrency (currency);
+			return value.ToString ($"F{marketStatus.PriceAssetPrecision}");
+
+		}
+
+	}
+
+}
