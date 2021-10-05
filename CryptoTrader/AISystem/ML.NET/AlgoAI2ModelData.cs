a39d849cb1561c8766e27398bf9b333920544957
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-05 16:50:45 +0200

    Added data unit

---
+++ CryptoTrader/AISystem/ML.NET/AlgoAI2ModelData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+
+namespace CryptoTrader.AISystem.ML.NET {
+	public class AlgoAI2ModelData {
+
+		[ColumnName ("PastPrices")]
+		[LoadColumn (0, 9999)]
+		[VectorType (10000)]
+		public float[] PriceData { get; set; }
+
+		[ColumnName ("Confidence")]
+		[LoadColumn (10000)]
+		public float HoldConfidence { get; set; }
+
+	}
+}
