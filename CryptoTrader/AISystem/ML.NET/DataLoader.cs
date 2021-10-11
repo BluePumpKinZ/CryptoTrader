667fcdbbbf1e23a636595fc948359608fe11f5f2
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-11 23:04:24 +0200

    Added function for converting to ML.NET readable data

---
+++ CryptoTrader/AISystem/ML.NET/DataLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace CryptoTrader.AISystem.ML.NET {
+
+	public class DataLoader {
+
+		public static AlgoAI2ModelData[] ConvertToModelData(double[][] inputs, double[][] outputs) {
+
+			if (inputs.Length != outputs.Length)
+				throw new ArgumentException ("Argument dimensions do not match.");
+
+			int length = outputs.Length;
+
+			AlgoAI2ModelData[] output = new AlgoAI2ModelData[length];
+			for (int i = 0; i < length; i++) {
+				AlgoAI2ModelData dataItem = new AlgoAI2ModelData ();
+				dataItem.PriceData = inputs[i].Cast<float> ().ToArray ();
+				dataItem.HoldConfidence = (float)outputs[i][0];
+			}
+			return output;
+
+		}
+
+	}
+
+}
