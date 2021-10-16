ce62f182243e5c5160fdcda128f845ddca8cf817
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-16 20:18:12 +0200

    Made model input and output classes

--- CryptoTrader/AISystem/ML.NET/AlgoAI2ModelInput.cs
+++ CryptoTrader/AISystem/ML.NET/AlgoAI2ModelInput.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.ML.Data;
+using CryptoTrader.Algorithms;
+using System;
 
 namespace CryptoTrader.AISystem.ML.NET {
 	public class AlgoAI2ModelInput {
 
 		[ColumnName ("PastPrices")]
-		[LoadColumn (0, 9999)]
-		[VectorType (10000)]
+		[LoadColumn (0, AIDataConversion.INPUT_LAYER_SAMPLES - 1)]
+		[VectorType (AIDataConversion.INPUT_LAYER_SAMPLES)]
 		public float[] PriceData { get; set; }
 
-		[ColumnName ("Confidence")]
-		[LoadColumn (10000)]
+		[ColumnName ("HoldConfidence")]
+		[LoadColumn (AIDataConversion.INPUT_LAYER_SAMPLES)]
 		public float HoldConfidence { get; set; }
+
+		public static AlgoAI2ModelInput FromDoubleArray (double[] values) {
+			if (values is null || values.Length != AIDataConversion.INPUT_LAYER_SAMPLES)
+				throw new ArgumentException ($"Values array cannot be null and must be have a size of {AIDataConversion.INPUT_LAYER_SAMPLES}.", nameof (values));
+			AlgoAI2ModelInput toReturn = new AlgoAI2ModelInput ();
+			toReturn.PriceData = new float[values.Length];
+			for (int i = 0; i < values.Length; i++) {
+				toReturn.PriceData[i] = (float)values[i];
+			}
+			return toReturn;
+		}
 
 	}
 }
--- CryptoTrader/AISystem/ML.NET/AlgoAI2ModelOutput.cs
+++ CryptoTrader/AISystem/ML.NET/AlgoAI2ModelOutput.cs
@@ -1,8 +1,11 @@
-﻿namespace CryptoTrader.AISystem.ML.NET {
+﻿using Microsoft.ML.Data;
+
+namespace CryptoTrader.AISystem.ML.NET {
 
 	public class AlgoAI2ModelOutput {
 
-		
+		[ColumnName ("HoldConfidence")]
+		public float HoldConfidence { get; set; }
 
 	}
 
